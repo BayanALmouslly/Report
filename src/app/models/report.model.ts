72bb52cee868f5d7ff9550dda59d7bef411cd628
bayanmsl.bm <bayanmsl.bm@gmail.com>
2021-05-13 04:54:02 +0300

    add print

--- src/app/models/report.model.ts
+++ src/app/models/report.model.ts
@@ -4,7 +4,9 @@ export class Report {
     constructor() { }
     ClientName: string = ""
     ProducerName: string = ""
+    ProducerImage:any
     Count: number = 0
     price: number = 0
     date:Data=new Date()
+
 }
--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -10,12 +10,13 @@ import { Report } from '../models/report.model';
 export class ReportsComponent implements OnInit {
 
   constructor(public sanitizer: DomSanitizer,) { }
-  headers: string[] = [' العميل', ' المنتج', 'الكمية', 'السعر', 'التاريخ', '']
+  headers: string[] = [' العميل', ' المنتج', 'صورة المنتج', 'الكمية', 'السعر', 'الوقت والتاريخ', '']
   orders: Report[] = []
   Order: Report = new Report()
   ngOnInit(): void {
   }
   addOrder() {
+    console.log(this.Order)
     this.orders.push(this.Order)
     this.Order = new Report()
     this.sumSalary()
@@ -30,8 +31,37 @@ export class ReportsComponent implements OnInit {
     this.orders.forEach(o => {
       this.counts = 0
       this.prices = 0
-      this.prices += o.price
-      this.counts += o.Count
+      this.prices += o.price * o.Count
+      //this.counts += o.Count
     })
+  }
+  imagePath: any
+  onFileChanged(event: any, order: Report) {
+    const files = event.target.files;
+    if (files.length === 0)
+      return;
+
+    const mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      return;
+    }
+
+    const reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      order.ProducerImage = reader.result;
+    }
+  }
+  print() {
+   
+   var divToPrint = document.getElementById('componentID');
+  var newWin = window.open('', 'Print-Window');
+  newWin?.document.open();
+  newWin?.document.write('<html><style>.container{direction: rtl; text-align: center;padding-top: 20px;}</style><link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" media="print"/><body onload="window.print()">' + divToPrint?.innerHTML + '</body></html>');
+  newWin?.document.close();
+  setTimeout(function() {
+    newWin?.close();
+  }, 10);
   }
 }
